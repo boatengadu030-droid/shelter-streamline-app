1ffa386cfa18b3d17699fc52255f5419199d6e39
boatengadu030-droid <boatengadu030@gmail.com>
2026-05-06 10:39:30 +0000

    Delete supabase directory

--- supabase/migrations/20260505135833_5ccc0b3b-e56d-4336-8e3e-b055c8c5f66d.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE public.children REPLICA IDENTITY FULL;
-ALTER TABLE public.sponsors REPLICA IDENTITY FULL;
-ALTER TABLE public.donations REPLICA IDENTITY FULL;
-ALTER TABLE public.inventory_items REPLICA IDENTITY FULL;
-ALTER TABLE public.compliance_records REPLICA IDENTITY FULL;
-ALTER TABLE public.events REPLICA IDENTITY FULL;
-ALTER PUBLICATION supabase_realtime ADD TABLE public.children, public.sponsors, public.donations, public.inventory_items, public.compliance_records, public.events;
